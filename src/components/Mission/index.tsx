52004b8083da79394676f93ad0f251245634a2ea
Hye <hye@hpcnt.com>
2021-01-24 01:45:36 +0900

    feat: render img only flickr image existed

--- src/components/Mission/index.tsx
+++ src/components/Mission/index.tsx
@@ -23,11 +23,18 @@ const Mission: React.FC<Record<'launch', Launch>> = ({
       alignItems="center"
       className="root"
     >
-      <img className="image" src={flickr_images[0]} alt={rocket_name} />
-      <h2>{mission_name}</h2>
+      {flickr_images[0] && (
+        <img className="image" src={flickr_images[0]} alt={rocket_name} />
+      )}
+      <h2 className="title">{mission_name}</h2>
+      <div className="description">
+        {rocket_name}
+        <br />
+        launched at&nbsp;
+        {launch_date_local.split('T').join(' ')}
+      </div>
       <Link to={video_link} name="youtube link" />
       <Link to={article_link} name="article link" />
-      {launch_date_local.split('T').join(' ')}
     </Grid>
   );
 };
