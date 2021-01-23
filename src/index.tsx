c7f6ea1ea440d6e82554ca39001f02cda5034a8b
Hye <hye@hpcnt.com>
2021-01-24 00:34:27 +0900

    add: apollo client + ApolloProvider to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import { ApolloProvider } from '@apollo/client';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import Client from './utils/apollo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={Client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
