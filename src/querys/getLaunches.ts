3c33797af0a04e09df9c7300bbc1198dcbfb6afb
Hye <hye@hpcnt.com>
2021-01-24 00:35:14 +0900

    add: getLaunches query

---
+++ src/querys/getLaunches.ts
@@ -0,0 +1,22 @@
+import { gql } from 'apollo-boost';
+
+const getLaunches = gql`
+  {
+    launches {
+      id
+      rocket {
+        rocket_name
+        rocket_type
+      }
+      mission_name
+      launch_date_local
+      links {
+        video_link
+        article_link
+        flickr_images
+      }
+    }
+  }
+`;
+
+export default getLaunches;
